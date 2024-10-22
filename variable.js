82f1a40f7afc92ccd71afe8c16049fb8b7466888
이진현 <61403140+lightochre@users.noreply.github.com>
2024-10-22 17:24:05 +0900

    Add files via upload

--- variable.js
+++ variable.js
@@ -19,18 +19,53 @@ console.log(globaName);
 //var hoisting (끌어올리다)
 //var no block scope
 
-//3.Constants - 값을 선언함과 동시에 변경할수 없다.
-//favor immutable data type always for a few reasons;
-// - securify [보안]
-// -thread safety
-// -reduce human mistakes
+//3.숫자타입
+//3.1 템플릿 리터럴 -문자열표기법, 백틱(``)를 사용한다.
+//var template = `Template literal`;
+var template = `<ul>
+    <li></li>
+</ul>`;
+console.log(template);
 
-//4.variable types
-//primitive, sigile item : number, string, boolean, null, underfiden, symbol
-//object, box container
-//function, first-class function
+//일반문자열 내에서는 줄바꿈이 허용되지 않는다.
+//이스케이프 시퀀스를 사용
+// var str = 'Hello
+// world';
+var _str = 'Hello \n world';
+console.log(_str);
 
-const count = 17; //integer- 정수
-const size = 17.1; //decimal number - 소수점
-console.log(`value: ${count}, type: ${typeof count}`);
-console.log(`value: ${size}, type: ${typeof size}`);
+var frist = 'Ung-mo';
+var last = 'Lee';
+// console.log(`My name is` + frist +  ''  +  last  +  `.`);
+console.log(`My name is ${frist} ${last}`); //ES6 표현식
+console.log(`1+2 = ${1+3}`);
+
+// 4.동적타이핑
+//자바스트립트는 데이터 타입의 값을 자유롭게 할당 할 수 있다.
+
+var foo;
+console.log(typeof foo);
+
+foo = 3;
+console.log(typeof foo);
+
+foo = 'Hellow';
+console.log(typeof foo);
+
+foo = true;
+console.log(typeof foo);
+
+foo = null;
+console.log(typeof foo);
+
+foo = Symbol(); //Symbol
+console.log(typeof foo);
+
+foo = {} //객체
+console.log(typeof foo); //object
+
+foo = [] //배열
+console.log(typeof foo); //object
+
+foo = function() {}; //함수
+console.log(typeof foo);
